ddab3aa55e75fb03b18a60093f7008771f9e3a7f
Jonathan Goboli <jgoboli2@gmail.com>
2024-11-28 23:07:56 +0800

    Add Hero component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      App
+      <Hero />
     </main>
   );
 };
