67091e9994172cbeb40fdad27f5fa13ebcb889aa
Jonathan Goboli <jgoboli2@gmail.com>
2024-12-04 10:14:53 +0800

    Add Navbar component and integrate About section into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Hero />
+      <Navbar />
+      <section>
+        <Hero />
+      </section>
+      <section>
+        <About />
+      </section>
     </main>
   );
 };
