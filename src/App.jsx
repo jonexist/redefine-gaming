5bd90035acda2bad77c52c28ab9499969deb0b1f
Jonathan Goboli <jgoboli2@gmail.com>
2024-12-10 21:26:00 +0800

    Refactor App component to include Story, Contact, and Footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Story from "./components/Story";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
-      <section>
-        <Hero />
-      </section>
-      <section>
-        <About />
-      </section>
-      <section>
-        <Features />
-      </section>
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 };
